9ce892b2d9511f616745f8e48bbcb4bd5c98dc5f
Alan <eusoualancarvalho@gmail.com>
2025-01-21 17:36:25 -0300

    Adicionados testes unitários e controle para remoção do arquivo do bucket em caso de erro

--- deploy/deploy-aws.sh
+++ deploy/deploy-aws.sh
@@ -4,18 +4,11 @@
 # Este script simula os passos realizados no workflow de CD
 
 EKS_CLUSTER_NAME=app-cluster
-DB_INSTANCE_IDENTIFIER=burgers-db
 API_GATEWAY_NAME=http-api
 COGNITO_USER_POOL=customer-logins
-DATABASE_NAME=postgres
-DATABASE_USER=burger
-DB_PW=Burgers2024
 
-APP_IMAGE=gomesrodris/architect-burgers:0.0.8
+APP_IMAGE=dockerhubalancarvalho/fiap-videoslice-svc-engine:0.0.8
 
-MERCADOPAGO_POS_ID=Dummy  # Set if needed
-MERCADOPAGO_USER_ID=Dummy
-MERCADOPAGO_ACCESS_TOKEN=DummyDummy
 
 TMP_OUTPUTS=/tmp/deploy/tmp_outputs.vars
 
@@ -32,12 +25,6 @@ then
   exit 1
 fi
 
-./utils/get-database-url.sh ${DB_INSTANCE_IDENTIFIER} > $TMP_OUTPUTS
-if [ $? -ne 0 ]
-then
-  echo "Error checking AWS resource. See error messages"
-  exit 1
-fi
 source $TMP_OUTPUTS
 
 ./utils/get-external-api-info.sh ${API_GATEWAY_NAME} > $TMP_OUTPUTS
@@ -56,29 +43,6 @@ then
 fi
 source $TMP_OUTPUTS
 
-cat ./k8s/db/db-configs-cloud-template.yml \
-   | sed "s/{DB_URL}/${DB_URL}/" \
-   | sed "s/{DB_NAME}/${DATABASE_NAME}/" \
-   | sed "s/{DB_USER}/${DATABASE_USER}/" \
-   | sed "s/{DB_PASS}/${DB_PW}/" > /tmp/deploy/db-configs.yml
-
-if [ $? -ne 0 ]
-then
-  echo "Error preparing configmap. See error messages"
-  exit 1
-fi
-
-cat ./k8s/app/pagamento-configs-template.yml \
-   | sed "s|{APP_URL}|${API_URL}|" \
-   | sed "s/{POS_ID}/${MERCADOPAGO_POS_ID}/" \
-   | sed "s/{USER_ID}/${MERCADOPAGO_USER_ID}/" \
-   | sed "s/{ACCESS_TOKEN}/${MERCADOPAGO_ACCESS_TOKEN}/" > /tmp/deploy/pagamento-configs.yml
-
-if [ $? -ne 0 ]
-then
-  echo "Error preparing configmap. See error messages"
-  exit 1
-fi
 
 cat ./k8s/app/aws-configs-template.yml \
    | sed "s/{ACCESS_KEY_ID}/$(echo -n ${aws_access_key_id} | tr -d '\n')/" \
@@ -100,19 +64,6 @@ cat ./k8s/app/app-deployment.yml \
 
 aws eks update-kubeconfig --name ${EKS_CLUSTER_NAME}
 
-kubectl apply -f /tmp/deploy/db-configs.yml
-if [ $? -ne 0 ]
-then
-  echo "Error running kubectl step. See error messages"
-  exit 1
-fi
-
-kubectl apply -f /tmp/deploy/pagamento-configs.yml
-if [ $? -ne 0 ]
-then
-  echo "Error running kubectl step. See error messages"
-  exit 1
-fi
 
 kubectl apply -f /tmp/deploy/aws-configs.yml
 if [ $? -ne 0 ]
--- docker/build-local.sh
+++ docker/build-local.sh
@@ -2,13 +2,48 @@
 #
 # Cria a imagem docker localmente usando build container
 
-version=0.0.6
+repoprefix=dockerhubalancarvalho
+basename=fiap-videoslice-svc-engine
 
 cd $(dirname $0) || exit 1
-
 cd ..
 
-docker build . -f docker/Dockerfile.fullBuild -t gomesrodris/architect-burgers:$version
+version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
-docker push docker.io/gomesrodris/architect-burgers:$version
+if [ "$version" == "" ]
+then
+  echo "Could not get project version"
+  exit 1
+fi
 
+mvn clean install dependency:copy-dependencies
+#mvn -DskipTests -DskipITs clean install dependency:copy-dependencies
+
+if [ $? -ne 0 ]
+then
+  echo "Build error"
+  exit 2
+fi
+
+if [ -d ./docker/temp_libs ]
+then
+  rm ./docker/temp_libs/*
+else
+  mkdir ./docker/temp_libs || exit 1
+fi
+
+cp ./modules/web-app/target/*webapi-*.jar ./docker/temp_libs/ && cp ./modules/web-app/target/lib/* ./docker/temp_libs/
+
+cd docker || exit 1
+
+docker build . -t $repoprefix/$basename:$version
+
+if [ $? -ne 0 ]
+then
+  echo "Image build error"
+  exit 2
+fi
+
+echo "::: Built $repoprefix/$basename:$version"
+
+docker push docker.io/$repoprefix/$basename:$version
