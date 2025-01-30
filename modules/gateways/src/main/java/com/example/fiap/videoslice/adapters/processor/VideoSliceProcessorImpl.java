933ca003ef30e3ecf92cd101aa3a907bc2c8f4e4
Alan <eusoualancarvalho@gmail.com>
2025-01-30 20:37:53 -0300

    Adicionados testes unitários e controle para remoção do arquivo do bucket em caso de erro

--- modules/gateways/src/main/java/com/example/fiap/videoslice/adapters/processor/VideoSliceProcessorImpl.java
+++ modules/gateways/src/main/java/com/example/fiap/videoslice/adapters/processor/VideoSliceProcessorImpl.java
@@ -22,17 +22,23 @@ public class VideoSliceProcessorImpl implements VideoProcessor {
 
     @Override
     public File videoSlice(Video video, int periodInSeconds) throws ApplicationException {
-
+        System.out.println("VideoSliceProcessorImpl - videoSlice");
         try {
             Path tempDir = Files.createTempDirectory("frames");
+            System.out.println("VideoSliceProcessorImpl - videoSlice 1");
             Integer time = LocalDateTime.now().getNano();
 
+            System.out.println("VideoSliceProcessorImpl - videoSlice 2");
             String fileName = "frames_id_" + video.getId() + "__" + time + ".zip";
 
+            System.out.println("VideoSliceProcessorImpl - videoSlice 3");
             File file = new File(tempDir.toFile(), fileName);
+            System.out.println("VideoSliceProcessorImpl - videoSlice 4");
             try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(file))) {
+                System.out.println("VideoSliceProcessorImpl - videoSlice 5");
                 extractFrames(video.getPath(), periodInSeconds, zipOutputStream);
             }
+            System.out.println("VideoSliceProcessorImpl - videoSlice success");
             return file;
 
         }catch (IOException e){
@@ -41,10 +47,13 @@ public class VideoSliceProcessorImpl implements VideoProcessor {
     }
 
     public void extractFrames(String videoFilePath, int periodInSeconds, ZipOutputStream zipOutputStream) throws IOException, ApplicationException {
-
+        System.out.println("VideoSliceProcessorImpl - extractFrames 1");
         try (FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(videoFilePath)) {
+            System.out.println("VideoSliceProcessorImpl - extractFrames 2");
             frameGrabber.start();
+            System.out.println("VideoSliceProcessorImpl - extractFrames 3");
             Java2DFrameConverter converter = new Java2DFrameConverter();
+            System.out.println("VideoSliceProcessorImpl - extractFrames 4");
             int frameNumber = 0;
 
             while (true) {
@@ -52,23 +61,28 @@ public class VideoSliceProcessorImpl implements VideoProcessor {
                 if (frame == null) {
                     break;
                 }
-
+                System.out.println("VideoSliceProcessorImpl - extractFrames 5");
                 BufferedImage bufferedImage = converter.convert(frame);
                 if (bufferedImage == null) {
                     continue;
                 }
-
+                System.out.println("VideoSliceProcessorImpl - extractFrames 6");
                 String fileName = "frame" + frameNumber++ + ".png";
                 ZipEntry zipEntry = new ZipEntry(fileName);
+                System.out.println("VideoSliceProcessorImpl - extractFrames 7");
                 zipOutputStream.putNextEntry(zipEntry);
-                ImageIO.write(bufferedImage, "png", zipOutputStream);
-                zipOutputStream.closeEntry();
 
+                System.out.println("VideoSliceProcessorImpl - extractFrames 8");
+                ImageIO.write(bufferedImage, "png", zipOutputStream);
+                System.out.println("VideoSliceProcessorImpl - extractFrames 9");
+                zipOutputStream.closeEntry();
+                System.out.println("VideoSliceProcessorImpl - extractFrames 10");
                 frameGrabber.setTimestamp(frameGrabber.getTimestamp() + periodInSeconds * 1000000L);
             }
-
+            System.out.println("VideoSliceProcessorImpl - extractFrames 11");
             frameGrabber.stop();
         } catch (Exception e) {
+            System.out.println("VideoSliceProcessorImpl - Error extracting - " + e.toString());
             throw new ApplicationException("Error extracting the frames from the video. Could not open the file.");
         }
     }
--- modules/usecases/src/main/java/com/example/fiap/videoslice/domain/usecases/VideoUseCases.java
+++ modules/usecases/src/main/java/com/example/fiap/videoslice/domain/usecases/VideoUseCases.java
@@ -25,17 +25,20 @@ public class VideoUseCases {
 
     @Transactional
     public String executeVideoSlice(Video video, Integer frameTime) {
+        System.out.println("executeVideoSlice 1");
         String uploadedFilePath = "";
         File file = null;
         try {
             video = video.setVideoStatusToInProcess();
+            System.out.println("executeVideoSlice - Atualizar updateStatusVideo");
             updateStatusVideo(video);
-
+            System.out.println("executeVideoSlice - extractFramesFromVideo");
             file = extractFramesFromVideo(video, frameTime);
-
+            System.out.println("executeVideoSlice - saveFile");
             uploadedFilePath = saveFile(file);
-
+            System.out.println("executeVideoSlice - Fase 1 ok");
         } catch (ApplicationException e) {
+            System.out.println("Erro Fase 1 - " + e.toString());
             JSONObject messageJson = new JSONObject(video.getVideoJson());
             messageJson.put("status", StatusVideo.PROCESSED_ERROR.toString());
             messageJson.put("message", e.getMessage());
@@ -44,38 +47,46 @@ public class VideoUseCases {
         }
 
         try {
-
+            System.out.println("Fase 2 - setVideoFrameFilePath");
             Video videoSuccess = video.setVideoFrameFilePath(uploadedFilePath).setVideoStatusToProcessedSuccess();
+            System.out.println("Fase 2 - updateStatusVideo");
             updateStatusVideo(videoSuccess);
 
         } catch (ApplicationException e) {
+            System.out.println("Erro Fase 2 - " + e.toString());
             deleteFile(uploadedFilePath);
             return "error";
         }
+        System.out.println("executeVideoSlice - Fase 2 ok");
         return "success";
     }
 
     private File extractFramesFromVideo(Video video, int periodInSeconds) throws ApplicationException {
+        System.out.println("Method - extractFramesFromVideo");
         return this.videoGateway.videoSlice(video, periodInSeconds);
 
     }
 
     private String saveFile(File file) throws ApplicationException {
+        System.out.println("Method - saveFile");
         return this.videoGateway.saveFile(file);
     }
 
     public String updateStatusVideo(Video video) throws ApplicationException {
+        System.out.println("Method - updateStatusVideo");
         this.videoEventMessagingGateway.updateStatusVideo(video);
         return "ok";
     }
 
     public void notifyErrorProcessingTheVideo(String message) {
+        System.out.println("Method - notifyErrorProcessingTheVideo");
         JSONObject messageJson = new JSONObject(message);
         messageJson.put("status", StatusVideo.PROCESSED_ERROR.toString());
         this.videoEventMessagingGateway.notifyErrorProcessingTheVideo(messageJson.toString());
     }
 
     public void deleteFile(String filePath) {
+        System.out.println("Method - deleteFile");
         this.videoGateway.deleteFile(filePath);
 
     }
